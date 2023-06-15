61b577162180bf8c47fa5bed363b6bfe6f869638
ChadiElHanchi <chadielhanchi@gmail.com>
2023-06-15 07:46:17 -0700

    sorting

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
