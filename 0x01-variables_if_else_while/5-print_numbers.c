edbca1332e7c0bbd7bffd963d5e0410c235fe979
ChadiElHanchi <chadielhanchi@gmail.com>
2023-06-16 07:20:40 -0700

    sorting

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - alphabet
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char n;
+for (n = '0'; letter <= '10'; n++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
