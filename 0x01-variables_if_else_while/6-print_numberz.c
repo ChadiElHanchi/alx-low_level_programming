f37dba8586074cf8c5fc49c1809a69e2741d2099
ChadiElHanchi <chadielhanchi@gmail.com>
2023-06-16 07:42:19 -0700

    sorting

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - alphabet
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+int  n;
+for (n = 0; n <= 9; n++)
+{
+putchar('0' + n);
+}
+putchar('\n');
+return (0);
+}
