e05f9af83a6ae940787d0e88041d7a540581fc1f
ChadiElHanchi <chadielhanchi@gmail.com>
2023-06-16 07:54:36 -0700

    sorting

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - alphabet
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char n;
+for (n = 0; n <= 9; n++)
+{
+putchar(n);
+}
+for (digit = 'a'; digit <= 'f'; digit++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
