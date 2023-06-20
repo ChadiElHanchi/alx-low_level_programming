f4becf5600c2a6b00d22db790f77f6d47d4fb74f
ChadiElHanchi <chadielhanchi@gmail.com>
2023-06-20 11:22:12 -0700

    letter

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 /**
 * main - fibonacci <3
-*
-* Purpose - 
-*
-* Return:  (Success)
+* Purpose -
+*Return:  (Success)
 */
 int main(void)
 {
-		unsigned long int i;
-		unsigned long int bef = 1;
-		unsigned long int aft = 2;
-		unsigned long int l = 1000000000;
-		unsigned long int bef1;
-		unsigned long int bef2;
-		unsigned long int aft1;
-		unsigned long int aft2;
-
-
-		printf("%lu", bef);
-
-
+unsigned long int i;
+unsigned long int bef = 1;
+unsigned long int aft = 2;
+unsigned long int l = 1000000000;
+unsigned long int bef1;
+unsigned long int bef2;
+unsigned long int aft1;
+unsigned long int aft2;
+printf("%lu", bef);
 		for (i = 1; i < 91; i++)
 		{
 			printf(", %lu", aft);
@@ -46,5 +40,4 @@ int main(void)
 		}
 		printf("\n");
 		return (0);
-	}
-
+}
