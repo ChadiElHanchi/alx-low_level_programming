8fcc6d2e3c29a1a44bdf133feca49ad4a24fcda2
ChadiElHanchi <chadielhanchi@gmail.com>
2023-06-20 10:40:38 -0700

    letter

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,29 +5,31 @@
  */
 void times_table(void)
 {
-int i, j, result;
-for (i = 0; i <= 9; i++)
-{
-for (j = 0; j <= 9; j++)
-{
-result = i * j;
-if (j == 0)
-_putchar(result + '0');
-else if (result <= 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(result + '0');
+    int i, j, result;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            result = i * j;
+            if (j == 0)
+                _putchar(result + '0');
+            else if (result <= 9)
+            {
+                _putchar(',');
+                _putchar(' ');
+                _putchar(' ');
+                _putchar(result + '0');
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+                _putchar((result / 10) + '0');
+                _putchar((result % 10) + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }
-else
-{
-_putchar(',');
-_putchar(' ');
-_putchar((result / 10) + '0'); 
-_putchar((result % 10) + '0');
-}
-}
-_putchar('\n');
-}
-}
+
