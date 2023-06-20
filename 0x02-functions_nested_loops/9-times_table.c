a783118ab37022b47594d08d18922830dc8f92b7
ChadiElHanchi <chadielhanchi@gmail.com>
2023-06-20 10:35:13 -0700

    letter

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,14 +17,11 @@ void times_table(void)
             else if (result <= 9)
             {
                 _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
                 _putchar(result + '0');
             }
             else
             {
                 _putchar(',');
-                _putchar(' ');
                 _putchar((result / 10) + '0');
                 _putchar((result % 10) + '0');
             }
