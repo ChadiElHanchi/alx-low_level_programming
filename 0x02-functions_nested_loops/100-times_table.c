b28ac27d25746b1137f9122f28767197121a33ac
ChadiElHanchi <chadielhanchi@gmail.com>
2023-06-20 11:09:50 -0700

    letter

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,30 +6,14 @@
  */
 void print_number(int num)
 {
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
 	if (num < 10)
-	{
-		_putchar(',');
 		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(num + '0');
-	}
-	else if (num >= 10 && num < 100)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
+	else
 		_putchar((num / 10) + '0');
-		_putchar((num % 10) + '0');
-	}
-	else if (num >= 100)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar((num / 100) + '0');
-		_putchar(((num / 10) % 10) + '0');
-		_putchar((num % 10) + '0');
-	}
+	_putchar((num % 10) + '0');
 }
 
 /**
@@ -44,7 +28,7 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			_putchar(i + '0');
+			_putchar('0');
 			for (j = 1; j <= n; j++)
 			{
 				print_number(i * j);
