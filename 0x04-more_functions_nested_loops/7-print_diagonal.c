614f2835a2406175798fa7bb51b5608a5471dc9d
ChadiElHanchi <chadielhanchi@gmail.com>
2023-06-22 10:04:52 -0700

    C

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,16 +7,22 @@
  */
 void print_diagonal(int n)
 {
-int b, space;
 if (n <= 0)
+{
 _putchar('\n');
+}
 else
 {
-for (b = 1; b <= n; b++)
+int i, j;
+for (i = 0; i < n; i++)
 {
-for (space = 1; space <= b; space++)
+for (j = 0; j < n; j++)
+{
+if (j == i)
+_putchar('\\');
+else if (j < i)
 _putchar(' ');
-_putchar(92);
+}
 _putchar('\n');
 }
 }
