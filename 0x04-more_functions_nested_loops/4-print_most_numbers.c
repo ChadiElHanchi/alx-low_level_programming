53949c1dc2d68d765de64250d3e9bf40897f73cb
ChadiElHanchi <chadielhanchi@gmail.com>
2023-06-22 08:46:03 -0700

    C

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,12 @@ void print_most_numbers(void)
 int i = 0;
 int a = 2;
 int b = 4;
-while (i >= 0 && i <= 9 && i != a && i != b)
+while (i >= 0 && i <= 9)
+{
+if (i != a && i != b)
 {
 _putchar(i + 48);
+}
 i++;
 }
 _putchar('\n');
