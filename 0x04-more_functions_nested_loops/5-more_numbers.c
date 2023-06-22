9c4ff1dcba3f8f80b821bd65fda69ffabc4a2ebc
ChadiElHanchi <chadielhanchi@gmail.com>
2023-06-22 08:57:27 -0700

    C

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,17 @@
  */
 void more_numbers(void)
 {
-int i = 0;
-while (i >= 0 && i <= 14)
+int i, j;
+for (j = 0; j < 10; j++)
 {
-_putchar((i + 48) * 10);
-i++;
+for (i = 0; i <= 14; i++)
+{
+if (i > 9)
+{
+_putchar((i / 10) + '0');  // Print the tens digit
+}
+_putchar((i % 10) + '0');  // Print the ones digit
 }
 _putchar('\n');
+}
 }
