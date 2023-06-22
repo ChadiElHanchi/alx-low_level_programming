37e1669142413b26652f683cfe38ac147a61ac6d
ChadiElHanchi <chadielhanchi@gmail.com>
2023-06-22 08:50:21 -0700

    C

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+int i = 0;
+while (i >= 0 && i <= 14)
+{
+_putchar((i + 48)*10);
+i++;
+}
+_putchar('\n');
+}
