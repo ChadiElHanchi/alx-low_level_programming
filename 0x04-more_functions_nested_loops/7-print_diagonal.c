de063f955d4b2d681b37af80d8047728e4da0c30
ChadiElHanchi <chadielhanchi@gmail.com>
2023-06-22 09:16:57 -0700

    C

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ *
+ * @n: Number of times the character '_' should be printed
+ */
+void print_diagonal(int n)
+{
+int b;
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (b = 1; b <= n; b++)
+_putchar('$');
+_putchar('\n');
+}
+}
