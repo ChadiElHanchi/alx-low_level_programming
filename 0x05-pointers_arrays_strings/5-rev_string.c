09e8311a2b42be9ec7ee8cbc5503a2a967f2cd58
ChadiElHanchi <chadielhanchi@gmail.com>
2023-06-26 08:53:12 -0700

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,16 +10,13 @@
 
 void rev_string(char *s)
 {
-int length = 0;
-int i;
-
-if (s == NULL)
-	return;
-while (s[length] != '\0')
-{
-	length++;
-}
-for (i = length - 1; i >= 0; i--)
-putchar (s[i]);
-putchar ('\n');
+	int l,i;
+	charc temp;
+	for (l =0; s[l] != '\0'; ++l)
+		for (i = 0; i < l / 2; i++)
+		{
+			temp = s[i];
+			s[i] = s[l - 1 - i];
+			s[l - 1 - i] = temp;
+		}
 }
