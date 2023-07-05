6a8a757d3a7ffc196ede4fcd433ad710ef2da5d5
ChadiElHanchi <chadielhanchi@gmail.com>
2023-07-05 08:16:33 -0700

    recur

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - fact
+ * @n: pointer string
+ * Return: Always 0 (Success)
+ */
+int factorial(int n)
+{
+if( n < 0 )
+return (-1);
+}
+else if (n == 0)
+{
+return (1);
+}
+return (n * factorial(n - 1));
+}
