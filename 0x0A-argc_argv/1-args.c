a53eaff9ab0d9fcf2db4128e24ccc31ec93062e4
ChadiElHanchi <chadielhanchi@gmail.com>
2023-07-07 08:55:20 -0700

    arg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,36 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
-* main - coins
-* @argc: int
-* @argv: list
+* main - number of arg
+* @argc: cmd line 
+* @argv: pointer
 * Return: 0
 */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-if (argc == 2)
-{
-int i, leastcents = 0, money = atoi(argv[1]);
-int cents[] = {25, 10, 5, 2, 1};
-
-for (i = 0; i < 5; i++)
-{
-if (money >= cents[i])
-{
-leastcents += money / cents[i];
-money = money % cents[i];
-if (money % cents[i] == 0)
-{
-break;
-}
-}
-}
-printf("%d\n", leastcents);
-}
-else
-{
-printf("Error\n");
-return (1);
-}
+printf("%d\n", argc - 1);
 return (0);
 }
