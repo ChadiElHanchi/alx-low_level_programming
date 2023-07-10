76bdc0564e4e2d9f7cb1b9eb14b74451a888a236
ChadiElHanchi <chadielhanchi@gmail.com>
2023-07-10 11:38:05 -0700

    p

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
  * _strlen - length
  * @s: string
@@ -6,43 +8,39 @@
  */
 int _strlen(char *s)
 {
-{
 int size = 0;
+
 for (; s[size] != '\0'; size++)
 ;
 return (size);
 }
-
 /**
-* str_concat - concatenates strings
-* @s1: string
-* @s2: string
-@ Return: pointer
-*/
-
+ * str_concat - concatenates strings
+ * @s1: string
+ * @s2: string
+ * Return: pointer
+ */
 char *str_concat(char *s1, char *s2)
 {
 int size1, size2, i;
 char *m;
 
 if (s1 == NULL)
-s1 = "\0";
+s1 = "";
 if (s2 == NULL)
-s2 = "\0";
-
+s2 = "";
 size1 = _strlen(s1);
 size2 = _strlen(s2);
-m = malloc((size1 + size2) *sizeof(char) + 1);
-if (m == 0)
-return (0);
-for (i = 0; i <= size1 + size2; i++)
+m = malloc((size1 + size2 + 1) * sizeof(char));
+if (m == NULL)
+return (NULL);
+for (i = 0; i < size1 + size2; i++)
 {
 if (i < size1)
 m[i] = s1[i];
 else
-m[i} = s2[i - size1];
+m[i] = s2[i - size1];
 }
-m[i} = '\0';
-return (m):
+m[i] = '\0';
+return (m);
 }
-
