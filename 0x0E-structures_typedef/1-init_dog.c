c6fbd12418e4b9603c85fe3365004fad7c996e60
ChadiElHanchi <chadielhanchi@gmail.com>
2023-07-17 12:46:03 -0700

    dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,8 @@
 /**
 * init_dog - dog
 * @d: dog init
-* @name: dog name
+* @name: dog nam
+* @age: age
 * @owner: owner name
 * Return: void
 */
@@ -11,8 +12,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d)
 {
-d->name =name;
-d->age =age;
-d->owner =owner;
+d->name = name;
+d->age = age;
+d->owner = owner;
 }
 }
