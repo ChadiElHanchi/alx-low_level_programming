d84ce583a847b5d75c346f3e8c64837c52682849
ChadiElHanchi <chadielhanchi@gmail.com>
2023-07-27 17:01:51 -0700

    lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,9 +6,10 @@
 #include <string.h>
 /**
 * struct list_s - linked list
-* * @str: string
+* @str: string
 * @len: length
 * @next: point
+*
 * Description: singly linked node
 * for project
 */
@@ -19,12 +20,13 @@ unsigned int len;
 struct list_s *next;
 } list_t;
 
-int _putchar(charc c);
-int _strlen(charc *s);
+int _putchar(char c);
+int _strlen(char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 void _constructor(void) __attribute__ ((constructor));
+
 #endif
