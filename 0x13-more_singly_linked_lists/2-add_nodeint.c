f893d4a505c25f7f245c94ec372c5e56a149c566
ChadiElHanchi <chadielhanchi@gmail.com>
2023-08-01 16:12:23 -0700

    lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * add_nodeint - adds node
  * @head: pointer
- * @h: value node
+ * @n: value node
  *
  * Return: pointer
  */
@@ -13,10 +13,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	if (!head || !new_node)
 		return (NULL);
-	new_node->next = NULL;
-	new_node-> = n;
+new_node->next = NULL;
+new_node->n = n;
 	if (*head)
-		new_nodeè>next = *head;
+		new_node->next = *head;
 	*head = new_node;
 	return (new_node);
 }
