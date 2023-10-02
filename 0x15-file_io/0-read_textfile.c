872d5d57a80ee1e47ed74d35d8ee2af2fd198f85
ChadiElhanchi <chadielhanchi@gmail.com>
2023-10-02 23:30:09 +0100

    mm

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,7 +7,7 @@
  *    * @letters: number of letters to be read
  *     * Return: w- actual number of bytes read and printed
  *      *        0 when function fails or filename is NULL.
- */
+ *       */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 		char *buf;
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 					ssize_t t;
 
 						fd = open(filename, O_RDONLY);
-			if (fd == -1)
+							if (fd == -1)
 										return (0);
 								buf = malloc(sizeof(char) * letters);
 									t = read(fd, buf, letters);
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  *  * create_file - Creates a file.
  *   * @filename: A pointer to the name of the file to create.
@@ -5,7 +7,7 @@
  *     *
  *      * Return: If the function fails - -1.
  *       *         Otherwise - 1.
- */
+ *        */
 int create_file(const char *filename, char *text_content)
 {
 		int fd, w, len = 0;
