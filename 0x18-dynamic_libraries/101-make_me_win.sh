bbd588d225d271e1d45ffd0bb4356e2eef0b2caa
ChadiElHanchi <chadielhanchi@gmail.com>
2023-12-13 08:42:43 -0800

    l

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp_https://github.com/chadielhanchi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp_https://github.com/ChadiElHanchi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
